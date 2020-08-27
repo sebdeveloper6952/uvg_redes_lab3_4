99ea05bffd68968c8349b510c2dd7e02e2b77ae5
Sebastian Arriola <sebdeveloper6952@gmail.com>
2020-08-27 08:47:06 -0600

    dv simulacion terminada.

--- distance_vector.py
+++ distance_vector.py
@@ -46,7 +46,6 @@ class DVNode:
                 if cost_to_source + source_cost < self.table[to][id_from]:
                     self.table[to][id_from] = cost_to_source + source_cost
                     updated = True
-                    print(f'node {self.id} updating cost to {i} via {id_from} = {cost_to_source + source_cost}')
 
         return updated
 
@@ -66,76 +65,16 @@ class DVNode:
         
         return shortest_id
 
-    def print_shortest_paths(self):
+    def get_shortest_paths(self):
         """
         Se utiliza para debug.
         Imprime un resumen de las rutas que debe tomar este nodo
         para llegar a otros nodos, y el costo de dichas rutas.
         """
-        paths = [[INF, 0] for _ in range(self.num_nodes)]
+        paths = [[0, INF] for _ in range(self.num_nodes)]
         for to in range(self.num_nodes):
             for via in range(self.num_nodes):
-                if self.table[to][via] < paths[to][0]:
-                    paths[to] = [self.table[to][via], via]
-        print(f'i am node {self.id}')
-        for n in range(len(paths)):
-            print(f'my shortest path to {n} is via {paths[n][1]} with cost {paths[n][0]}')
-
-
-    def print_table(self):
-        """
-        Se utiliza para debug.
-        Imprime la tabla de ruteo de este nodo.
-        """
-        print(f'i am node {self.id} and my table is:')
-        for i in self.table:
-            print(i)
-        print('***************************************')
-
-# a = DVNode(0, [0, 3, 23, INF])
-# b = DVNode(1, [3, 0, 2, INF])
-# c = DVNode(2, [23, 2, 0, 5])
-# d = DVNode(3, [INF, INF, 5, 0])
-# nodes = [a,b,c,d]
-
-# neighbors = [
-#     [1,2],
-#     [0,2],
-#     [0,1,3],
-#     [2]
-# ]
-
-# print('simulation start state')
-# a.print_table()
-# b.print_table()
-# c.print_table()
-# d.print_table()
-# print('*************************************')
-
-# queue = []
-# for i in range(len(neighbors)):
-#     for n in neighbors[i]:
-#         queue.append([i, n])
-
-# while len(queue) > 0:
-#     current = queue.pop(0)
-#     print(f'processing {current}')
-#     if nodes[current[0]].update_table(current[1], nodes[current[1]].table):
-#         for n in neighbors[current[0]]:
-#             if n is not current[1]:
-#                 queue.append([n, current[0]])
-#                 print(f'update ocurred, enqueuing {n}, {current[0]}')
-    
-
-# print('*************************************')
-# print('simulation finish state')
-# a.print_table()
-# b.print_table()
-# c.print_table()
-# d.print_table()
-
-# print('******** shortest paths **********')
-# a.print_shortest_paths()
-# b.print_shortest_paths()
-# c.print_shortest_paths()
-# d.print_shortest_paths()
+                if self.table[to][via] < paths[to][1]:
+                    paths[to] = [via, self.table[to][via]]
+        
+        return paths
--- dv_client.py
+++ dv_client.py
@@ -5,13 +5,14 @@ from distance_vector import DVNode, INF
 from select import select
 from time import sleep
 
+MSG_SIZE = 2048
+
 class DVClient:
     def __init__(self, host, port, node_id, neighbors):
         self.host = host
         self.port = int(port)
         self.my_id = int(node_id)
         self.node = DVNode(self.my_id, neighbors)
-        print(f'Nodo {self.node.id}: mis vecinos son los nodos: {self.node.neighbors}')
         self.init_socket()
         self.run_node()
 
@@ -22,7 +23,7 @@ class DVClient:
         # enviar mensaje de login
         msg = json.dumps({"type": 101, "id": self.my_id})
         self.socket.sendall(msg.encode('utf-8'))
-        data = self.socket.recv(1024)
+        data = self.socket.recv(MSG_SIZE)
         data = data.decode('utf-8').replace('\'', '\"')
         dec_msg = json.loads(data)
         if dec_msg['type'] == 102:
@@ -32,96 +33,69 @@ class DVClient:
             exit(1)
 
     def run_node(self):
+        # enviar tabla a vecinos
+        for n in self.node.neighbors:
+            table_msg = {"type": 0, "table": self.node.table}
+            msg = {"type": 103, "idSender": self.node.id, "idReciever": n, "message": table_msg, "p": ""}
+
+            size = len(json.dumps(msg).encode('utf-8'))
+            msg["p"] = '0' * (MSG_SIZE - size)
+            
+            msg = json.dumps(msg)
+            msg = msg.encode('utf-8')
+            self.socket.sendall(msg)
+            sleep(1)
+        
+        iters = 0
         while True:
             # revisar si hay mensajes por leer
             to_read, _, _ = select([self.socket], [], [], 2.0)
             
             # si hay mensajes pendientes, procesar
             if to_read:
-                data = self.socket.recv(512)
-                if data:
-                    print(f'Nodo {self.node.id}: hay data, leyendo...')
-                    data = data.decode('utf-8')
-                    print('********* DATA **********')
-                    print(data)
-                    print('*************************')
-                else:
-                    print(f'Nodo {self.node.id} no hay data para leer.')
-
-            # pausa
-            sleep(1 + self.node.id)
-
-            # enviar tabla a vecinos
-            for n in self.node.neighbors:
-                table_msg = {"type": 0, "table": self.node.table}
-                msg = {"type": 103, "idSender": self.node.id, "idReciever": n, "message": table_msg, "p": ""}
-
-                size = len(json.dumps(msg).encode('utf-8'))
-                msg["p"] = '0' * (512 - size)
+                data = self.socket.recv(MSG_SIZE)
+                if len(data) == 0:
+                    continue
                 
-                msg = json.dumps(msg)
-                msg = msg.encode('utf-8')
-                self.socket.sendall(msg)
-                print(f'Nodo {self.node.id}: enviando mi tabla a nodo {n}, size: {len(msg)}')
-                sleep(1 + self.node.id)
+                data = data.decode('utf-8').replace('\'', '\"')
+                data = json.loads(data)
+                if data['message']['type'] == 0:
+                    if self.node.update_table(data['idSender'], data['message']['table']):
+                        print(f'Nodo {self.node.id}: actualizando mi tabla...')
+                        # reiniciar numero de iteraciones sin actualizaciones
+                        iters = 0
+                        
+                        # enviar tabla a vecinos
+                        for n in self.node.neighbors:
+                            table_msg = {"type": 0, "table": self.node.table}
+                            msg = {"type": 103, "idSender": self.node.id, "idReciever": n, "message": table_msg, "p": ""}
+
+                            size = len(json.dumps(msg).encode('utf-8'))
+                            msg["p"] = '0' * (MSG_SIZE - size)
+                            
+                            msg = json.dumps(msg)
+                            msg = msg.encode('utf-8')
+                            self.socket.sendall(msg)
+                            sleep(1)
+            
+            iters += 1
+
+            if iters == 10:
+                print(f'Nodo {self.node.id} esta listo para enviar mensajes...')
+                break
+        
+        # escribir tablas de cada nodo a archivo para revision
+        with open(f'./output/dv_table_{self.node.id}.txt', 'w') as f:
+            paths = self.node.get_shortest_paths()
+            paths_s = ""
+            for i in range(len(paths)):
+                paths_s += 'Hacia '+str(i)+' me voy por '+str(paths[i][0])+' con costo '+str(paths[i][1])+'\n'
+            f.write(paths_s)
+
+        while True:
+            print(f'Nodo {self.node.id} durmiendo...')
+            sleep(1)
 
     def close_socket(self):
         self.socket.close()
         print(f'Nodo {self.node.id}: cerrando conexion con servidor.')
-        
-
-
-
-# creacion de este nodo
-# node = DVNode(my_id, neighbors)
-# print(f'Nodo {node.id}: mis vecinos son los nodos: {node.neighbors}')
-
-# # creacion de socket y conexion a servidor
-# socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# socket.connect((host, port))
-# print(f'Nodo {node.id}: conectado al servidor.')
-
-# # enviar mensaje de login
-# msg = json.dumps({"type": 101, "id": my_id})
-# socket.sendall(msg.encode('utf-8'))
-# data = socket.recv(1024)
-# data = data.decode('utf-8').replace('\'', '\"')
-# dec_msg = json.loads(data)
-# if dec_msg['type'] == 102:
-#     print(f'Nodo {node.id}: ha iniciado sesion.')
-# else:
-#     print(f'Nodo {node.id}: error al iniciar sesion.')
-#     exit(1)
-
-# while True:
-#     # revisar si hay mensajes por leer
-#     to_read, _, _ = select([socket], [], [], 2.0)
-    
-#     # si hay mensajes pendientes, procesar
-#     if to_read:
-#         data = socket.recv(1024)
-#         if data:
-#             print(f'Nodo {node.id}: hay data, leyendo...')
-#             data = data.decode('utf-8')
-#             print(data)
-#         else:
-#             print(f'Nodo {node.id} no hay data para leer.')
-
-#     # pausa
-#     sleep(1 + node.id)
-
-#     # enviar tabla a vecinos
-#     for n in node.neighbors:
-#         msg = {"type": 103, "idSender": node.id, "idReciever": n, "message": "ola"}
-#         msg = json.dumps(msg)
-#         print(f'Nodo {node.id}: enviando mi tabla a nodo {n}')
-#         socket.sendall(msg.encode('utf-8'))
-#         sleep(1 + node.id)
-
-
-# # loop de simulacion
-# # for n in node.neighbors:
-# #     print(f'Nodo {node.id}: enviando mi tabla a nodo {n}')
-    
-# socket.close()
-# print(f'Nodo {node.id}: se desconecto de servidor.')
